ab785b0cb3d321d603397b09d92376d577e75e40
danavaaa <154718957+danavaaa@users.noreply.github.com>
2026-04-18 11:20:48 +0700

    setup project dengan supabase

---
+++ android/app/src/main/kotlin/com/example/smartpaud/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.smartpaud
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/core/config/supabase_config.dart
@@ -0,0 +1,5 @@
+class SupabaseConfig {
+  static const String url = 'https://vcwvdgmrydztxpccirxb.supabase.co';
+  static const String anonKey =
+      'sb_publishable_ANq88IGV9M-qLLw_LrN-BQ_Qj2_jrkt';
+}
