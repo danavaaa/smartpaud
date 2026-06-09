53ef3e8571a1eb560b53b8a518f41cafabe7f47a
danavaaa <154718957+danavaaa@users.noreply.github.com>
2026-06-09 19:48:55 +0700

    rev overflow kelola data siswa

--- lib/features/admin/siswa/siswa_form_page.dart
+++ lib/features/admin/siswa/siswa_form_page.dart
@@ -237,7 +237,7 @@ class _SiswaFormPageState extends State<SiswaFormPage> {
       ),
 
       // Isi halaman
-      body: Padding(
+      body: SingleChildScrollView(
         // Memberi jarak isi dari tepi layar
         padding: const EdgeInsets.fromLTRB(20, 12, 20, 20),
         child: Column(
