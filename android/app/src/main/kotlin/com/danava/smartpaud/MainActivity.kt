cb2fc2ce46d6211c094bd157ed4e328ce3de12c6
danavaaa <154718957+danavaaa@users.noreply.github.com>
2026-06-14 22:52:42 +0700

    perbaiki MainActivity package Android

---
+++ android/app/src/main/kotlin/com/danava/smartpaud/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.danava.smartpaud
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/example/smartpaud/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.smartpaud
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
